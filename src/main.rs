c22b7125fa8c4cd69eb5f78b4857d15411b067a2
Maarten Ectors <maarten@ectors.com>
2025-07-10 07:31:03 +0100

    updated version

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use anyhow::bail;
 #[command(
     name = "greentic", 
     about = "The Greener Agentic AI", 
-    version = "0.1.0"
+    version = "0.2.0"
 )]
 struct Cli {
     #[command(subcommand)]
